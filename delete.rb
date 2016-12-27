806e240e744f74af26aa2934623b6b823de3c5ad
divya <sjdivya03@gmail.com>
2016-12-27 10:57:12 +0530

    delete

---
+++ delete.rb
@@ -0,0 +1,13 @@
+class Hash
+	  def delete_value(value)
+	    delete_if { |k,v| v == value }
+	    puts "Finded value is #{value}"
+
+	  end
+	end
+
+	h = {'apple' => 'green', 'potato' => 'red', 'sun' => 'yellow',
+	     'katydid' => 'green' }
+
+	h.delete_value('green')
+	  puts"After deleting hash element containing matching value \n #{h}"
