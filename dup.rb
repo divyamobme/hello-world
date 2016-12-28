5551c2e96490541e4a7c58d3e23361e54b459703
divya <sjdivya03@gmail.com>
2016-12-28 12:12:05 +0530

    duplicates removed

--- dup.rb
+++ dup.rb
@@ -2,10 +2,17 @@
 class Duplicate
 def my_uniq()
 	values = Array[4, 1, 2, 2, 3, 3]
-puts "#{values}", "\n"
+puts "Array \t #{values}", "\n"
+prev='xxxxx'
+new_array=[]
+values.sort.each do |current|
+	if (current != prev)
+		new_array << current
+		prev=current
+	end
+end
+puts "Without Duplicates #{new_array}"
 
-values.uniq!
-puts "#{values}", "\n"
 
 end
 end
